33d8f105c97cca12db7d343d51604fa25ad9940f
Maks <maksovni@gmail.com>
2021-03-25 21:10:39 +0000

    created multimedia class

---
+++ src/main/java/uk/ac/soton/comp1206/scene/Multimedia.java
@@ -0,0 +1,17 @@
+package uk.ac.soton.comp1206.scene;
+
+import javafx.scene.media.MediaPlayer;
+
+public class Multimedia{
+    MediaPlayer audioPlayer;
+    MediaPlayer musicPlayer;
+
+    public void playAudio(){
+
+    }
+
+    public void playBackgroundMusic(){
+
+    }
+    
+}
